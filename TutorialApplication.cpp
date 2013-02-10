9421e746485e178c193a90b2c7163405abc66bf7
Lucas Fryzek <Nercos7@gmail.com>
2013-02-10 16:30:46 -0500

    initial commit

---
+++ TutorialApplication.cpp
@@ -0,0 +1,85 @@
+/*
+-----------------------------------------------------------------------------
+Filename:    TutorialApplication.cpp
+-----------------------------------------------------------------------------
+
+This source file is part of the
+   ___                 __    __ _ _    _ 
+  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+/ \_// (_| | | |  __/  \  /\  /| |   <| |
+\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+      |___/                              
+      Tutorial Framework
+      http://www.ogre3d.org/tikiwiki/
+-----------------------------------------------------------------------------
+*/
+#include "TutorialApplication.h"
+
+//-------------------------------------------------------------------------------------
+TutorialApplication::TutorialApplication(void)
+{
+}
+//-------------------------------------------------------------------------------------
+TutorialApplication::~TutorialApplication(void)
+{
+}
+
+//-------------------------------------------------------------------------------------
+void TutorialApplication::createScene(void){
+
+	//Set scene ambient lighting
+	mSceneMgr->setAmbientLight(Ogre::ColourValue(0.25,0.25,0.25));
+
+	//Create p1 entity
+	Ogre::Entity *player1 = mSceneMgr->createEntity("player1","ninja.mesh");
+	Ogre::SceneNode *node = mSceneMgr->getRootSceneNode()->createChildSceneNode("player1");
+	node->attachObject(player1);
+
+	//Create light cause light is cool
+	Ogre::Light *light = mSceneMgr->createLight("Light1");
+	light->setType(Ogre::Light::LT_POINT);
+	light->setPosition(Ogre::Vector3(250,150,250));
+	light->setDiffuseColour(Ogre::ColourValue::White);
+	light->setSpecularColour(Ogre::ColourValue::White);
+
+	//create scene node
+}
+
+
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+    INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
+#else
+    int main(int argc, char *argv[])
+#endif
+    {
+        // Create application object
+        TutorialApplication app;
+
+        try {
+            app.go();
+        } catch( Ogre::Exception& e ) {
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+            MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+            std::cerr << "An exception has occured: " <<
+                e.getFullDescription().c_str() << std::endl;
+#endif
+        }
+
+        return 0;
+    }
+
+#ifdef __cplusplus
+}
+#endif
---
+++ makefile
@@ -0,0 +1,17 @@
+SRCS = $(wildcard *.cpp)
+OBJS = $(patsubst %.cpp,%.o,$(SRCS))
+LD_FLAGS = `pkg-config --libs OGRE OGRE-Terrain OIS` -lboost_system 
+CC_FLAGS = -g `pkg-config --cflags OGRE OGRE-Terrain OIS` 
+TITLE = tut4
+
+$(TITLE): $(OBJS)
+	g++ -o $@ $^ $(LD_FLAGS)
+%.o: %.cpp
+	g++ $(CC_FLAGS) -c $<
+
+clean:
+	rm *.o
+	rm $(TITLE)
+
+run:
+	./$(TITLE)
